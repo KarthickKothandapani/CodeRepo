c0e7b28d89000ccfe61d9a363857fcf529dab7b9
Karthick K <Karthick.Kothandapane@gmail.com>
2022-08-17 07:38:05 +0530

    This is my first commit - base line code

---
+++ CICDTestAPI/Controllers/TestAPIController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CICDTestAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestAPIController : ControllerBase
+    {
+        [HttpGet(Name = "HelloWorld")]
+        public ActionResult<String> HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
